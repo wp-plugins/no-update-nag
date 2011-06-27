8dc5cd23933205f8723e8abf4781b75f6315616a
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-27 22:35:26 +0000

    * Update to v1.2 * Add function c2c_no_update_nag() * Hook c2c_no_update_nag() into 'admin_init' action rather than use create_function() (as was done prior to v1.1) * Note compatibility through WP 3.2+ * Minor code formatting changes (spacing) * Fix plugin homepage and author links in description in readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/no-update-nag/tags/1.2@401872 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- no-update-nag.php
+++ no-update-nag.php
@@ -1,49 +1,30 @@
 <?php
+/**
+ * @package No_Update_Nag
+ * @author Scott Reilly
+ * @version 1.2
+ */
 /*
 Plugin Name: No Update Nag
-Version: 1.1.1
-Plugin URI: http://coffee2code.com/wp-plugins/no-update-nag
+Version: 1.2
+Plugin URI: http://coffee2code.com/wp-plugins/no-update-nag/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Removes the WordPress update nag that appears at the top of all admin pages when a new version of WordPress is released
 
-Tired of WordPress nagging you about a new release?  Maybe you already know about it and want to hold
-off on an update and would rather not see the update nag on every admin page you visit.  Activate this
-plugin and be bothered no more!
+Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+, 3.1+, 3.2+.
 
-Of course, the status of your version of WordPress (be it current or out of date) is still reflected in
-the footer of your admin pages.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://coffee2code.com/wp-plugins/no-update-nag/
 
-And certainly of course I don't advocate completely ignoring the fact that updates often contain fixes
-for critical bugs or exploits.  However, there are various other means of learning about updates; and
-once known, not everyone is able or willing to update immediately.
-
-Tips:
-
-There are probably two primary ways in which I foresee this plugin being used.
-
-1. Simply activate it and leave it active.  You can still check the footer of your WordPress admin pages
-to see if your version is out-of-date (if you dont' already discover that fact via your WordPress
-dashboard, blog, news sites, WP involvement, etc).
-
-2. Leave the plugin deactivated.  Once you've learned about an update to WordPress, activate the plugin to
-remove the nag until such time as you do the update, then deactivate the plugin once again.
-
-Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/no-update-nag.zip and unzip it into your
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
+TODO:
+	* Add filter to define capabilities for who can see update nag (or perhaps who can't see it)
 
 */
 
 /*
-Copyright (c) 2008-2010 by Scott Reilly (aka coffee2code)
+Copyright (c) 2008-2011 by Scott Reilly (aka coffee2code)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
@@ -59,5 +40,17 @@ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
-add_action( 'admin_init', create_function('', 'remove_action( \'admin_notices\', \'update_nag\', 3 );') );
+if ( ! function_exists( 'c2c_no_update_nag' ) ) :
+/**
+ * Disable the WordPress update nag
+ *
+ * @since 1.2
+ */
+function c2c_no_update_nag() {
+	remove_action( 'admin_notices', 'update_nag', 3 );
+}
+endif;
+
+add_action( 'admin_init', 'c2c_no_update_nag' );
+
 ?>
